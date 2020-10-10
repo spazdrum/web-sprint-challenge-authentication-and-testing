d16799d693ed7a0585016a0137dbb305c781b622
spazdrum <spazdrum@gmail.com>
2020-10-09 22:12:16 -0400

    C:/Program Files/Git/register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("./auth-model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secret = require("./secret");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  const { username, password } = req.body;
+  const rounds = process.env.BCRYPT_ROUNDS || 4;
+
+  db.add({ username, password: bcrypt.hashSync(password, rounds) })
+    .then((user) => {
+      res.status(201).json({ message: `Welcome ${username}`, token });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error adding new user" });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
