47fb7e9f6577e6cc792cd9a7e6c5688f25e6a788
spazdrum <spazdrum@gmail.com>
2020-10-12 21:48:53 -0400

    register and login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,6 +20,20 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "Invalid credentials!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
