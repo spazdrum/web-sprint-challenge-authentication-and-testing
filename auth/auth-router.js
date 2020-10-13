ad15e5ce18feaee2507f5d5fdac1cdca85de7a8b
spazdrum <spazdrum@gmail.com>
2020-10-12 21:42:33 -0400

    Login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secret");
+const Users = require("../user/user-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
